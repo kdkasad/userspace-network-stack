f95e7cac837ae10a5eb388b6514355114b095c58
Kian Kasad <kian@kasad.com>
2025-04-29 18:53:43 -0400

    tun: Support setting running state of TUN device

--- examples/tun_create.rs
+++ examples/tun_create.rs
@@ -1,10 +1,13 @@
-use std::process::ExitCode;
+use std::{io::Read, process::ExitCode};
 
-use unet::tun::TunDevice;
+use unet::tun::{NetworkInterface, TunDevice};
 
+/// Creates a TUN device and sets its state to running.
+/// If the first argument is `--wait`, it will wait for user input after creating the device but
+/// before exiting to allow other programs to inspect/modify the interface.
 pub fn main() -> ExitCode {
     // Attempt to create a new TUN device
-    let tundev = match TunDevice::create(None) {
+    let mut tundev = match TunDevice::create(None) {
         Ok(dev) => dev,
         Err(err) => {
             eprintln!("Failed to create TUN device: {}", err);
@@ -13,10 +16,20 @@ pub fn main() -> ExitCode {
     };
 
     // Print the name of the TUN device
-    println!(
-        "New TUN device named {} created",
-        tundev.name().to_string_lossy()
-    );
+    println!("New TUN device named {} created", tundev.name());
+
+    // Enable interface
+    if let Err(err) = tundev.set_running(true) {
+        eprintln!("Failed to set {} up: {}", tundev.name(), err);
+    }
+    println!("Set {} running state to up", tundev.name());
+
+    // Wait if requested
+    if std::env::args().nth(1).is_some_and(|arg| arg == "--wait") {
+        println!("Waiting for user input before exiting.");
+        let mut buf = [0u8; 1];
+        let _ = std::io::stdin().read(&mut buf).unwrap();
+    }
 
     ExitCode::SUCCESS
 }
--- src/tun.rs
+++ src/tun.rs
@@ -1,18 +1,22 @@
 //! Utilities for dealing with TUN devices
 
 use std::{
-    ffi::{CStr, CString},
+    ffi::CStr,
     fs::{File, OpenOptions},
     ops::{Deref, DerefMut},
-    os::fd::AsRawFd,
+    os::fd::{AsRawFd, FromRawFd, OwnedFd, RawFd},
 };
 
+use libc::{
+    __c_anonymous_ifr_ifru, AF_INET, IFF_TUN, IFF_TUN_EXCL, IFF_UP, Ioctl, SIOCGIFFLAGS,
+    SIOCSIFFLAGS, SOCK_DGRAM, TUNSETIFF, c_short, c_ushort, ifreq, ioctl, socket,
+};
 use zerocopy::IntoBytes;
 
 /// Represents a TUN device
 pub struct TunDevice {
     file: File,
-    name: CString,
+    name: String,
 }
 
 impl TunDevice {
@@ -44,18 +48,28 @@ impl TunDevice {
             .read(true)
             .write(true)
             .open("/dev/net/tun")?;
-        let ifname = ioctl_create_tun(file.as_raw_fd(), name_template, true)?;
+        let flags = u16::try_from(IFF_TUN | IFF_TUN_EXCL).unwrap();
+        let (ifname, _flags) =
+            ifreq_ioctl(file.as_raw_fd(), name_template, TUNSETIFF, Some(flags))?;
         Ok(Self { file, name: ifname })
-    }
-
-    /// Returns the name of the TUN interface
-    #[must_use]
-    pub fn name(&self) -> &CStr {
-        self.name.as_c_str()
     }
 }
 
-/// Allow dereferencing a [`TunDevice`] into a [`File`]
+impl NetworkInterface for TunDevice {
+    /// Returns the name of the TUN interface
+    #[must_use]
+    fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+impl AsRawFd for TunDevice {
+    fn as_raw_fd(&self) -> std::os::unix::prelude::RawFd {
+        self.file.as_raw_fd()
+    }
+}
+
+/// Support dereferencing a [`TunDevice`] into a [`File`]
 impl Deref for TunDevice {
     type Target = File;
     fn deref(&self) -> &Self::Target {
@@ -63,62 +77,108 @@ impl Deref for TunDevice {
     }
 }
 
-/// Allow dereferencing a [`TunDevice`] into a [`File`]
+/// Support dereferencing a [`TunDevice`] into a [`File`]
 impl DerefMut for TunDevice {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.file
     }
 }
 
-/// Performs the `ioctl(2)` call which creates a TUN device.
+/// Performs an `ioctl(2)` used to configure a network interface, specifically one which takes
+/// a `struct ifreq` pointer as the argument and makes use of the `ifr_name` and `ifr_flags`
+/// fields.
 ///
-/// The `fd` argument must be a file descriptor referring to `/dev/net/tun`.
+/// Returns the post-ioctl name and flags of the `struct ifreq`, as some ioctls modify those.
+/// See `netdevice(7)`.
 ///
 /// # Panics
 ///
-/// This function panics if `name_template` has a value which is longer than 15 bytes.
-fn ioctl_create_tun(
-    fd: i32,
-    name_template: Option<&str>,
-    exclusive: bool,
-) -> std::io::Result<CString> {
-    use libc::{
-        __c_anonymous_ifr_ifru, IFF_TUN, IFF_TUN_EXCL, TUNSETIFF, c_short, c_ushort, ifreq, ioctl,
-    };
-
+/// This function panics if `name` has a value which is longer than 15 bytes.
+fn ifreq_ioctl(
+    fd: RawFd,
+    name: Option<&str>,
+    request: Ioctl,
+    flags: Option<c_ushort>,
+) -> std::io::Result<(String, u16)> {
     #[allow(clippy::cast_possible_wrap)]
-    let flags =
-        c_ushort::try_from(IFF_TUN | if exclusive { IFF_TUN_EXCL } else { 0 }).unwrap() as c_short;
     let mut ifreq = ifreq {
         ifr_name: [0; 16],
-        ifr_ifru: __c_anonymous_ifr_ifru { ifru_flags: flags },
+        ifr_ifru: __c_anonymous_ifr_ifru {
+            ifru_flags: flags.unwrap_or(0) as c_short,
+        },
     };
 
-    if let Some(template) = name_template {
-        let bytes = template.as_bytes();
-        assert!(bytes.len() < ifreq.ifr_name.len());
+    if let Some(name_str) = name {
+        let name_bytes = name_str.as_bytes();
+        assert!(name_bytes.len() < ifreq.ifr_name.len());
         // SAFETY: template will exist, and the length is known to be less than the length of
         // the name field of the ifreq. Since the length of the template is strictly less than the
         // `ifr_name` field, the resulting `ifr_name` is guaranteed to be null-terminated.
         unsafe {
             std::ptr::copy_nonoverlapping(
-                bytes.as_ptr().cast::<i8>(),
+                name_bytes.as_ptr().cast::<i8>(),
                 ifreq.ifr_name.as_mut_ptr(),
-                bytes.len(),
+                name_bytes.len(),
             );
         }
     }
     let result = unsafe {
         // SAFETY: This ioctl request takes a pointer to a struct ifreq.
-        ioctl(fd, TUNSETIFF, &raw mut ifreq)
+        ioctl(fd, request, &raw mut ifreq)
     };
     if result == 0 {
         #[allow(clippy::cast_sign_loss)]
         let returned_name_buf = ifreq.ifr_name.as_bytes();
-        Ok(CStr::from_bytes_until_nul(returned_name_buf)
+        let name = CStr::from_bytes_until_nul(returned_name_buf)
             .expect("Returned interface name is not null-terminated")
-            .to_owned())
+            .to_str()
+            .expect("Returned interface name is not valid UTF-8")
+            .to_owned();
+        let flags = unsafe { ifreq.ifr_ifru.ifru_flags };
+        #[allow(clippy::cast_sign_loss)]
+        Ok((name, flags as c_ushort))
     } else {
         Err(std::io::Error::last_os_error())
+    }
+}
+
+pub trait NetworkInterface: AsRawFd {
+    /// Returns the name of the interface.
+    #[must_use]
+    fn name(&self) -> &str;
+
+    /// Sets the running state of the interface (i.e. the `IFF_UP`) flag.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `Err` if getting the current state or setting the new state fail.
+    fn set_running(&mut self, up: bool) -> std::io::Result<()> {
+        // Open a socket to perform the ioctl on
+        let socket = unsafe {
+            let fd = socket(AF_INET, SOCK_DGRAM, 0);
+            if fd < 0 {
+                return Err(std::io::Error::last_os_error());
+            }
+            OwnedFd::from_raw_fd(fd)
+        };
+        // Get current interface flags
+        let (_name, old_flags) =
+            ifreq_ioctl(socket.as_raw_fd(), Some(self.name()), SIOCGIFFLAGS, None)?;
+        // Modify and set new flags
+        let iff_up = c_ushort::try_from(IFF_UP).unwrap();
+        let new_flags = if up {
+            old_flags | iff_up
+        } else {
+            old_flags & !iff_up
+        };
+        if old_flags != new_flags {
+            ifreq_ioctl(
+                socket.as_raw_fd(),
+                Some(self.name()),
+                SIOCSIFFLAGS,
+                Some(new_flags),
+            )?;
+        }
+        Ok(())
     }
 }
